e1135869ef55e02d35153671f0298da3d0b2aa96
Aldo Yael Ramírez Pérez <ap6747852@gmail.com>
2026-07-28 15:31:19 -0600

    verificar edad

---
+++ Actividad_Selectivas/brindar_informacion.py
@@ -0,0 +1,14 @@
+#Brindar información
+consulta = input("Ingrese el nombre de artista, película o serie: "). lower()
+match consulta:
+    case "inception":
+        info = "Peñícula de ciencia ficción dirigida por Christopher Nolan."
+    case "beatles":
+        info = "Banda británica de rock formada en 1960."
+    case "rick and morty":
+        info = "Serie animada de comedia y cienci ficción."
+    case "avengers":
+        info = "Película de superhéroes del MCU."
+    case _:
+        info = "No se encontró información."
+print("información", info)
